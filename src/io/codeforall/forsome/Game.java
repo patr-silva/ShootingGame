25691e825ab0f525322697f3026f35ed703e17e8
patr-silva <patriciaresende.s92@gmail.com>
2024-06-05 23:35:07 +0100

    score board fixed

--- src/io/codeforall/forsome/Game.java
+++ src/io/codeforall/forsome/Game.java
@@ -3,12 +3,16 @@ package io.codeforall.forsome;
 import io.codeforall.forsome.characters.NormalEnemy;
 import io.codeforall.forsome.characters.Player;
 import io.codeforall.forsome.grid.Grid;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 
 public class Game {
-    public static int score = 20;
     private Grid grid;
-    private Collideable player;
+    private Text scoreBoard;
+    private Text healthBoard;
 
+    public static int score = 20;
+    private Collideable player;
     private Collideable enemy1;
     private Collideable enemy2;
 
@@ -16,6 +20,9 @@ public class Game {
 
     public Game(int width, int height, int delay) {
         this.grid = new Grid(width, height);
+        scoreBoard = new Text(5, 5, "");
+        healthBoard = new Text(5, 20, "");
+
         this.player = new Player(this.grid);
         this.delay = delay;
         this.enemy1 = new NormalEnemy(50, 15, this.grid, true, 5, 10);
@@ -31,12 +38,24 @@ public class Game {
             CollideableManager.detectCollisions();
             CollideableManager.show();
             CollideableManager.move();
+            this.scoreBoard();
             if (score < 0) {
                 player.kill();
-                System.out.println("F Player: " + score );
+                System.out.println("F Player: " + score);
             }
         }
     }
 
     //Create score as Text
+    public void scoreBoard() {
+        scoreBoard.setText("Score: " + score);
+        scoreBoard.setColor(Color.BLACK);
+
+        scoreBoard.draw();
+        healthBoard.setText("Health: " + score);
+        healthBoard.setColor(Color.BLACK);
+
+        healthBoard.draw();
+
+    }
 }
